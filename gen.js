e17ba6035c5854481b64dba4221fa0a9ec772284
tobi <tobi@tobias-huebner.org>
2020-11-28 17:49:23 +0100

    generator

--- gen.js
+++ gen.js
@@ -8,19 +8,27 @@ const lang_map = JSON.parse(fs.readFileSync("lang.json", "utf8"));
 
 const languages = ["de", "en"]
 
-fs.mkdir("content/lang", err => {
+
+fs.rmdir("content/lang", {recursive : true}, err => {
 	console.log(err);
+	fs.mkdir("content/lang", err => {
+		console.log(err);
+
+		for(let lang of languages){
+			for(let id in lang_map){
+				let qry = "#" + id + "-lang";
+				root.querySelector(qry).set_content(lang_map[id][lang])
+			}
+			console.log(root.toString())
+			let langdir = path.join("content/lang", lang);
+			fs.mkdir(langdir, {recursive : true}, err => {
+				console.log(err);
+			});
+			fs.writeFileSync(path.join(langdir, "index.html"), root.toString())
+		}
+
+	});
 });
 
-for(let lang of languages){
-	for(let id in lang_map){
-		let qry = "#" + id + "-lang";
-		root.querySelector(qry).set_content(lang_map[id][lang])
-	}
-	console.log(root.toString())
-	let langdir = path.join("content/lang", lang);
-	fs.mkdir(langdir, {recursive : true}, err => {
-		console.log(err);
-	});
-	fs.writeFileSync(path.join(langdir, "index.html"), root.toString())
-}
+
+
